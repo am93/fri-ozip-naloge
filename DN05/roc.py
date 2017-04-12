56dfe994217de0c58d980f2f93f266f2b77f17b3
Anze <anzze.medved@gmail.com>
2017-04-12 20:19:09 +0200

    [DN05] Code cleanup

--- DN05/roc.py
+++ DN05/roc.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score
 import random
 import csv
 
@@ -89,8 +88,6 @@ def compute_rho(real, class_prob, num_iter=1000):
             rho += 0.5
         iter += 1
 
-    print(iter)
-
     return rho/num_iter
 
 
@@ -125,24 +122,7 @@ def plot_results(tpr, fpr, auc, rho):
     plt.show()
 
 
-def do_testing():
-    """
-    Simple testing function
-    """
-    trueclass = [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0]
-    probs = [0.95, 0.95, 0.82, 0.82, 0.82, 0.73, 0.61, 0.61, 0.53, 0.38, 0.38, 0.11, 0.11]
-    tc = [random.randint(0, 1) for _ in range(20)]
-    tp = [random.uniform(0, 1) for _ in range(20)]
-    print('   AUROC simple: ', AUROC(trueclass, probs))
-    print('ROC AUC sklearn: ', roc_auc_score(trueclass, probs))
-    print('   RHO estimate: ', compute_rho(trueclass, probs))
-    print('   AUROC random: ', AUROC(tc, tp))
-    print('ROC AUC sklearn: ', roc_auc_score(tc, tp))
-    print('   RHO estimate: ', compute_rho(tc, tp))
-
-
 if __name__ == '__main__':
-    do_testing()
     y, prob = read_input_csv('roc-podatki.csv')
     auc = AUROC(y, prob)
     rho = compute_rho(y, prob)
---
+++ DN05/test.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import roc_auc_score
+
+def do_testing():
+    """
+    Simple testing function
+    """
+    trueclass = [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0]
+    probs = [0.95, 0.95, 0.82, 0.82, 0.82, 0.73, 0.61, 0.61, 0.53, 0.38, 0.38, 0.11, 0.11]
+    tc = [random.randint(0, 1) for _ in range(20)]
+    tp = [random.uniform(0, 1) for _ in range(20)]
+    print('   AUROC simple: ', AUROC(trueclass, probs))
+    print('ROC AUC sklearn: ', roc_auc_score(trueclass, probs))
+    print('   RHO estimate: ', compute_rho(trueclass, probs))
+    print('   AUROC random: ', AUROC(tc, tp))
+    print('ROC AUC sklearn: ', roc_auc_score(tc, tp))
+    print('   RHO estimate: ', compute_rho(tc, tp))
